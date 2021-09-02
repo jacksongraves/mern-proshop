7d6a49c4d0e929fc53c2c53173b43798fd0004f7
Jackson Graves <jackson.graves@me.com>
2021-09-02 10:22:46 -0500

    Configuring .env variables (.gitignored)

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 // Node.js modules for server setup
 const express = require("express");
+const dotenv = require("dotenv");
 
 // Data imports for testing purposes
 const products = require("./data/products");
 
-// Set up the API
+// Set up the API and environment variables
 const app = express();
+dotenv.config();
 
+// ----BEGIN ENDPOINTS----
+
+// Get root endpoint
 app.get("/", (req, res) => {
 	res.send("API is running...");
 });
@@ -22,4 +27,12 @@ app.get("/api/products/:id", (req, res) => {
 	res.json(product);
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+// ----END ENDPOINTS----
+
+// Launch the server and listen on target port
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
